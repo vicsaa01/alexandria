16d16dce425c7de1b8731176a11ca0089b904ca9
vicsaa01 <victorsaakes@gmail.com>
2025-04-01 11:58:20 +0200

    #043 - Home page reactive to session

--- frontend/src/app/home-page/home-page.component.ts
+++ frontend/src/app/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { DefaultTableComponent } from '../default-table/default-table.component';
 import { Client } from '../../client';
 import { JSONWebToken } from '../../jwt';
@@ -6,17 +7,25 @@ import { JSONWebToken } from '../../jwt';
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [DefaultTableComponent],
+  imports: [CommonModule, DefaultTableComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
 export class HomePageComponent {
+  loggedIn: boolean = false;
   recent: { _id: any; tag: string; views: number; lastViewedOn: string; dateAdded: string; url: string}[] = [];
   mostViewed: { _id: any; tag: string; views: number; lastViewedOn: string; dateAdded: string; url: string}[] = [];
 
   constructor(private client: Client, private jwt: JSONWebToken) {}
 
   ngOnInit(): void {
+    // Check if logged in
+    if (localStorage.getItem('sessionToken') != null && localStorage.getItem('userID') != null) {
+      this.loggedIn = true;
+    } else {
+      this.loggedIn = false;
+    }
+
     // Generate JWT token
     const token = this.jwt.createToken(60);
 
