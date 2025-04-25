e276712028bc3fad1d2e65be986bf4462712922f
vicsaa01 <victorsaakes@gmail.com>
2025-04-25 15:03:01 +0200

    Revert "#068 - Restart to .env changes"
    This reverts commit 8b9d6856809f7f0ad1c213d1c0cc8f1a97753961.

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,7 +1,7 @@
 
 export const environment = {
   production: false,
-  prodUrl: '',
-  jwtKey: '',
+  prodUrl: 'https://api-alexandria-1rqq.onrender.com',
+  jwtKey: 'dDlQOYga1SGvBPfD',
   // Add other variables as needed
 };
