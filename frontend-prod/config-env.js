b8eec2f0e8c8d675fc30e6e08e88ec4c4978a84e
vicsaa01 <victorsaakes@gmail.com>
2025-05-05 10:17:13 +0200

    #070 - (prod) added dotenv package

--- frontend-prod/config-env.js
+++ frontend-prod/config-env.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const dotenv = require('dotenv');
+const dotenv = require('dotenv').config();
 
 // Load environment variables from .env file
 const envConfig = process.env;
